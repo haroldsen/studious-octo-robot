324509315ed591d26b66c02610d6fb8046782f05
Christian Haroldsen <ctharoldsen@gmail.com>
2025-09-19 14:40:02 -0600

    Completed NPS 1.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const parkUrl = document.getElementById('park-url');
+parkUrl.innerHTML = parkData.fullName;
+parkUrl.href = parkData.url;
+document.title = `Basic Information - ${parkData.fullName}`;
+const bannerImg = document.getElementById('banner-img');
+bannerImg.src = parkData.images[0].url;
+bannerImg.alt = parkData.images[0].altText;
+document.getElementById('park-name').innerHTML = parkData.name;
+document.getElementById('park-states').innerHTML = parkData.states;

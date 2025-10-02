a47bc52d314ce331763f190d4a2a79b22a0c3c8f
Christian Haroldsen <ctharoldsen@gmail.com>
2025-10-02 10:34:22 -0600

    Made changes to NPS.

--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -1,4 +1,6 @@
 
+const baseUrl = "https://developer.nps.gov/api/v1/";
+
 const park = {
   id: "F58C6D24-8D10-4573-9826-65D42B8B83AD",
   url: "https://www.nps.gov/yell/index.htm",
@@ -202,6 +204,17 @@ export const parkInfoLinks = [
   }
 ];
 
-export function getParkData() {
-  return park;
+// export function getParkData() {
+//   return park;
+// }
+
+export async function getParkData() {
+  let data = {}
+  const response = await fetch(baseUrl + "parks" + "?parkCode=yell");
+  // check to make sure the reponse was ok.
+  if (response.ok) {
+    // convert to JSON
+    data = await response.json();
+  } else throw new Error("response not ok")
+    return data;
 }

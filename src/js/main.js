0f9a5f6b251381db7af941304568c54edb56d4da
Christian Haroldsen <ctharoldsen@gmail.com>
2025-10-03 00:18:59 -0600

    Made more progress on NPS site.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+
 import { getParkData } from "./parkService.mjs";
 import { mediaCardTemplate } from "./template.mjs";
 import { setHeaderFooter } from "./setHeaderFooter.mjs";
@@ -8,14 +9,15 @@ function setIntroData(data) {
 }
 
 function setInfoData(infoList) {
+    console.log(infoList.images[0]);
     const infoSection = document.getElementById('info');
-    for (let index = 0; index < infoList.length; index ++) {
-        infoSection.insertAdjacentHTML('beforeend', mediaCardTemplate(infoList[index]));
+    for (let index = 0; index < infoList.images.length; index ++) {
+        infoSection.insertAdjacentHTML('beforeend', mediaCardTemplate(infoList.images[index]));
     }
 }
 
 async function init() {
-    const parkData = await getParkData('glac');
+    const parkData = await getParkData('maca');
 
     setHeaderFooter(parkData);
     setIntroData(parkData);
